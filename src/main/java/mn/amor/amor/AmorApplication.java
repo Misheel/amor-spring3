832628efba7659daf30724636c147f7179bea878
erdenetsogt <a.erdenetsogt@gmail.com>
2019-07-29 12:13:36 +0800

    initial

---
+++ src/main/java/mn/amor/amor/AmorApplication.java
@@ -0,0 +1,13 @@
+package mn.amor.amor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmorApplication.class, args);
+	}
+
+}
